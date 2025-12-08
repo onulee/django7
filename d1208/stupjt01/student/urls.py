03b658d3fcf2bf2b9a23822fc0a7800e838f9f4a
onulee <onulee@naver.com>
2025-12-08 18:22:04 +0900

    디장고 student 프로젝트

--- d1208/stupjt01/student/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path,include
-from . import views
-
-# app_name : 닉네임,별칭
-app_name='student'
-urlpatterns = [
-    # write페이지 url로 들어와서 write함수 호출
-    path('write/', views.write,name='write'),
-]
--- d1208/stupjt01/student/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-# 성적입력페이지 열기
-def write(request):
-    return render(request,'write.html')
