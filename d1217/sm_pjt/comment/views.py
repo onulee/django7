dbb357c68f74cc7a6a9a37afed3a7dfcfa252011
onulee <onulee@naver.com>
2025-12-18 15:34:29 +0900

    댓글수정완료

--- d1217/sm_pjt/comment/views.py
+++ d1217/sm_pjt/comment/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from comment.models import Comment
 from board.models import Board
 from member.models import Member
+from django.forms.models import model_to_dict
 from django.http import JsonResponse,HttpResponse # 전송할때 Json타입으로 변경해서 전송
 from django.core import serializers  # Json타입으로 전달된 데이터를 파이썬데이터(set)로 변경
 
@@ -41,6 +42,10 @@ def cwrite(request):
         print(f"bno : {bno}, cpw : {cpw}, ccontent : {ccontent}")
         
         qs = Comment.objects.create(board=board,cpw=cpw,ccontent=ccontent,member=member)
+        # print("qs.cno : ",qs)
+        # dict_qs = model_to_dict(qs)
+        # context = {'comment':dict_qs,'result':'성공'}
+        
         # values()를 해야 list타입으로 변경됨.
         c_qs = list(Comment.objects.filter(cno=qs.cno).values())
         context = {'comment':c_qs[0],'result':'성공'}
