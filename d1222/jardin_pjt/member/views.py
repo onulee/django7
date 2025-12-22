be3ac2f3b21d264cfe3057880da2dd2488c4c093
onulee <onulee@naver.com>
2025-12-22 17:41:33 +0900

    id중복체크

--- d1222/jardin_pjt/member/views.py
+++ d1222/jardin_pjt/member/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from member.models import Member
 
 def step03(request):
     return render(request,'member/step03.html')
@@ -7,7 +8,10 @@ def step03(request):
 # json 리턴 : id가 존재하는지 확인
 def idCheck(request):
     # db확인
-    
+    id = request.GET.get('id','')
+    qs = Member.objects.filter(id=id)
+    if not qs: result = '사용가능'
+    else: result = '사용불가'    
     #----------------
-    context = {'result':'사용가능'}
+    context = {'result':result}
     return JsonResponse(context)
