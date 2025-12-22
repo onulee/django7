32f42e87879e7a16eb5b36c74e7f3f159addf1be
onulee <onulee@naver.com>
2025-12-22 17:21:00 +0900

    회원가입 아이디 체크 확인 수업

--- d1222/jardin_pjt/member/urls.py
+++ d1222/jardin_pjt/member/urls.py
@@ -3,7 +3,11 @@ from . import views
 
 app_name='member'
 urlpatterns = [
-    path('mwrite/', views.mwrite, name='mlist'),
+    # html리턴
+    path('step03/', views.step03, name='step03'),
+    
+    # JSON 리턴 : id가 존재하는지 체크
+    path('idCheck/', views.idCheck, name='idCheck'),
     
 ]
 
--- d1222/jardin_pjt/member/views.py
+++ d1222/jardin_pjt/member/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
-def mwrite(request):
+def step03(request):
     return render(request,'member/step03.html')
+
+# json 리턴 : id가 존재하는지 확인
+def idCheck(request):
+    # db확인
+    
+    #----------------
+    context = {'result':'사용가능'}
+    return JsonResponse(context)
