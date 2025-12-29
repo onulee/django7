95a049066c973cd1e34e51f01d60eb6b47c3c0d8
onulee <onulee@naver.com>
2025-12-29 15:54:47 +0900

    오라클 연결 수업

--- d1222/jardin_pjt/j_pjt/settings.py
+++ d1222/jardin_pjt/j_pjt/settings.py
@@ -67,14 +67,21 @@ WSGI_APPLICATION = 'j_pjt.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.oracle', 
+        'NAME': 'localhost:1521/xe',        
+        'USER': 'ora_user2',
+        'PASSWORD': '1111',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
